a9871fa31b1afe9ea325e7e5c42479832ee801f6
PoulaHelmy <43091492+PoulaHelmy@users.noreply.github.com>
2022-02-03 14:28:33 +0200

    solving app stating issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+
 import 'regenerator-runtime/runtime';
 import * as React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './ui/App';
 
 function initializeReactApp() {
   const appContainer = document.getElementById('appContainer');
   if (!appContainer) throw new Error('No #appContainer found in DOM');
-  render(React.createElement(App), appContainer);
+  ReactDOM.render(React.createElement(App), appContainer, () => {
+    console.log('React app initialized');
+  });
 }
 
 initializeReactApp();
